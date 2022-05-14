cec008b1be111ef47cfcc65b3acfe72312b60f17
nusbhchia <bhchia@nus.edu.sg>
2022-05-14 12:45:39 +0800

    uploading files
    ready for CSS 2

---
+++ data.js
@@ -0,0 +1,62 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3315168348179505,
+        "pitch": 0.046512463438364904,
+        "fov": 1.1989879464110744
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7980824847893526,
+          "pitch": -0.13815996705205436,
+          "rotation": 0.7853981633974483,
+          "target": "0-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9729224521793078,
+          "pitch": -8.608154544731406e-8,
+          "title": "Insert title here",
+          "text": "Insert description here"
+        },
+        {
+          "yaw": -1.9239320123627017,
+          "pitch": 0.20890693605961985,
+          "title": "Coffee?",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Pinnacle@Duxton",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
